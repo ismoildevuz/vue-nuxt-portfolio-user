cd8749373eba60d33ac3f930adee2dc0c31c6494
Ismoil4432 <ismoil4432@gmail.com>
2023-06-19 16:18:39 +0500

    projects section done

---
+++ components/ProjectCard.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="h-[400px] flex justify-between">
+    <div class="font-['Roboto_Slab'] my-5 mr-5">
+      <p class="flex gap-2 mb-5">
+        <span><Icons name="finished" /></span>
+
+        <span><Icons name="stick" /></span>
+
+        <span>Finished</span>
+      </p>
+
+      <h3 class="text-4xl font-black mb-5">
+        <span>{{ el.name }} </span>
+      </h3>
+
+      <h4 class="text-xl mb-8">
+        <span>
+          {{ el.description }}
+        </span>
+      </h4>
+
+      <div class="flex gap-10 items-center">
+        <a
+          target="_blank"
+          :href="el.link_project"
+          class="font-['DM_Sans'] font-bold py-3 px-10 bg-[#346259] rounded-lg"
+          >VISIT APP WEBSITE</a
+        >
+
+        <a
+          target="_blank"
+          :href="el.link_github"
+          class="bg-white w-10 h-10 flex items-center justify-center rounded-full overflow-hidden"
+        >
+          <Icons name="github" />
+        </a>
+      </div>
+    </div>
+
+    <img
+      class="rounded-lg w-[600px] h-[400px] object-cover select-none"
+      height="400"
+      width="600"
+      :src="`http://localhost:3001/api/image/file/${el.image?.file_name}`"
+      onerror="this.src='/images/project-1.png'"
+      alt=""
+    />
+  </div>
+</template>
+
+<script setup>
+defineProps(["el"]);
+</script>
+
+<style lang="scss" scoped></style>
--- components/Projects.vue
+++ components/Projects.vue
@@ -1,13 +1,122 @@
 <template>
   <section id="projects" class="section" data-id="home">
     <Container>
-      <div class="min-h-screen bg-zinc-700 my-5 p-5 scroll-mt-20">
-        <h1 class="text-center text-white">Projects</h1>
+      <div class="bg-black my-5 py-20">
+        <h2
+          class="text-center text-white text-3xl font-black font-['Roboto_Slab'] border-b-2 w-min mx-auto p-2"
+        >
+          Projects
+        </h2>
+
+        <div class="project-item my-10 px-20">
+          <div class="carousel overflow-hidden">
+            <div
+              class="carousel-inner flex duration-300 transition-transform ease-in-out"
+            >
+              <div
+                v-for="(el, ind) in data.list"
+                :key="ind"
+                class="carousel-item flex-0 flex-shrink-0 w-full"
+              >
+                <ProjectCard :el="el" />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="flex items-center justify-center gap-10">
+          <button
+            @click="prev"
+            class="carousel-prev rounded-full hover:bg-slate-800"
+          >
+            <Icons name="arrow_left" />
+          </button>
+
+          <div class="flex items-center justify-center gap-2">
+            <div
+              v-for="(el, ind) in data.list"
+              :key="ind"
+              :class="`w-2 h-2 bg-white opacity-${
+                ind == data.current ? '100' : '70'
+              } rounded-full`"
+            ></div>
+          </div>
+
+          <button
+            @click="next"
+            class="carousel-next rounded-full hover:bg-slate-800"
+          >
+            <Icons name="arrow_right" />
+          </button>
+        </div>
       </div>
     </Container>
   </section>
 </template>
 
-<script setup></script>
+<script setup>
+import axios from "axios";
 
-<style lang="scss" scoped></style>
+const data = reactive({
+  list: [],
+  current: 0,
+});
+
+const prev = () => {
+  if (data.current > 0) {
+    data.current--;
+    document.querySelector(`.carousel-inner`).style.transform = `translateX(-${
+      data.current * 100
+    }%)`;
+  }
+};
+
+const next = () => {
+  const totalItems = data.list.length;
+  if (data.current < totalItems - 1) {
+    data.current++;
+    document.querySelector(`.carousel-inner`).style.transform = `translateX(-${
+      data.current * 100
+    }%)`;
+  }
+};
+
+onMounted(() => {
+  axios
+    .get(`http://localhost:3001/api/project`)
+    .then((res) => {
+      data.list = res.data;
+    })
+    .catch((error) => {
+      const message = error?.response?.data?.message;
+      if (typeof message == "object") {
+        for (let i in message) {
+          setTimeout(() => {
+            ElNotification({
+              title: "Error",
+              message: message[i],
+              type: "warning",
+            });
+          }, i * 200);
+        }
+      } else {
+        ElNotification({
+          title: "Error",
+          message: message,
+          type: "warning",
+        });
+      }
+    });
+});
+</script>
+
+<style lang="scss" scoped>
+.project-item {
+  background: linear-gradient(
+    90deg,
+    rgba(52, 98, 89, 0.65) 0%,
+    rgba(52, 98, 89, 0.25) 20%,
+    rgba(52, 98, 89, 0) 100%
+  );
+}
+</style>
