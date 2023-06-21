0d8723348407fa1d4b174f4f1156c1fcc3161c17
Ismoil4432 <ismoil4432@gmail.com>
2023-06-21 15:23:55 +0500

    updated

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,8 @@
 <template>
   <Header />
-  <main class="min-h-scree bg-white dark:bg-black text-black dark:text-white">
+  <main
+    class="min-h-scree bg-white dark:bg-black text-black dark:text-white min-h-screen"
+  >
     <slot />
   </main>
   <Footer />
